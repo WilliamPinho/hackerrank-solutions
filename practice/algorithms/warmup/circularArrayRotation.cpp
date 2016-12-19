8cd8a5296ccc492bdc296429f11ff76da3c7cc2f
William Pinho <william.comp.ita@gmail.com>
2016-12-18 21:48:58 -0300

    Add solution to Circular Array Rotation problem

---
+++ practice/algorithms/warmup/circularArrayRotation.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int main() {
+  int N, K, Q, m;
+  cin >> N >> K >> Q;
+  K = K%N;                         // If K is bigger than N, then the number of rotations is K mod N
+  vector<int> v(N);
+  for(int i = 0; i < N; ++i) {
+    cin >> v[i];
+  }
+  while(Q > 0) {
+    cin >> m;
+    cout << v[(N-K+m)%N] << endl;  // Each element is shifted K positions to the right, which means that the new 0th element is the one originally in position (N - K) mod N.  
+    Q--;                           // and so the mth is the (N - K + m) mod N.
+  }
+  return 0;
+}
